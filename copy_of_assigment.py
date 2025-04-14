ac67d96f665ac911625e79dad8d4ddbab43d6b8f
sanket08ops <sanketsaraf08@gmail.com>
2025-04-14 16:18:21 +0530

    Update copy_of_assigment.py

--- copy_of_assigment.py
+++ copy_of_assigment.py
@@ -21,7 +21,7 @@ with st.sidebar:
     submit_keys = st.button("✅ Save API Keys")
 
 if submit_keys:
-    os.environ["WEATHER_API_KEY"] = WEATHER_API_KEY
+    os.environ["WEATHER_API_KEY"] = weather_api_key
     os.environ["TAVILY_API_KEY"] = tavily_api_key
     os.environ["GOOGLE_API_KEY"] = google_api_key
     st.success("✅ Keys saved! You can now ask about a place below.")
@@ -40,7 +40,7 @@ if all([weather_api_key, tavily_api_key, google_api_key]):
         if not api_key:
             return {"error": "Missing Weather API Key"}
 
-        url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={WEATHER_API_KEY}&units=metric"
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={weather_api_key}&units=metric"
         try:
             response = requests.get(url)
             data = response.json()
